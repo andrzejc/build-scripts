a8ff79e06842eb40df629b11274ab57fa117712d
Andrzej Ciarkowski <ciarkoa@amazon.com>
2016-03-13 15:53:55 +0100

    FindLIBPORTAUDIO.cmake: Be more systematic

--- cmake/FindLIBPORTAUDIO.cmake
+++ cmake/FindLIBPORTAUDIO.cmake
@@ -5,24 +5,36 @@
 #  LIBPORTAUDIO_LIBRARIES - The libraries needed to use portaudio
 #  LIBPORTAUDIO_DEFINITIONS - Compiler switches required for using portaudio
 
+set(LIBPORTAUDIO_SEARCH_LIBS libportaudio portaudio libportaudio-2 portaudio-2
+	libportaudio19 portaudio19 libportaudio-19 portaudio-19 libportaudio2
+	portaudio2)
+set(LIBPORTAUDIO_SEARCH_HEADERS portaudio.h)
+
 if (MSVC)
-	set(LIBPORTAUDIO_ROOT $ENV{LIBPORTAUDIO_ROOT} CACHE PATH "Installation folder of portaudio")
+	set(LIBPORTAUDIO_ROOT $ENV{LIBPORTAUDIO_ROOT} CACHE
+		PATH "Installation folder of portaudio")
+
 	if (CMAKE_CL_64)
-		set(LIBPORTAUDIO_DIR ${LIBPORTAUDIO_ROOT}/x64)
+		set(_subdirs ${LIBPORTAUDIO_ROOT}/x64 ${LIBPORTAUDIO_ROOT}/x64/lib
+			${LIBPORTAUDIO_ROOT}/lib/x64 ${LIBPORTAUDIO_ROOT}/lib64
+			${LIBPORTAUDIO_ROOT}/lib ${LIBPORTAUDIO_ROOT})
 	else ()
-		set(LIBPORTAUDIO_DIR ${LIBPORTAUDIO_ROOT}/x86)
+		set(_subdirs ${LIBPORTAUDIO_ROOT}/x86 ${LIBPORTAUDIO_ROOT}/x86/lib
+			${LIBPORTAUDIO_ROOT}/lib/x86 ${LIBPORTAUDIO_ROOT}/lib32
+			${LIBPORTAUDIO_ROOT}/lib ${LIBPORTAUDIO_ROOT})
 	endif()
 
 #	message("LIBPORTAUDIO_DIR: ${LIBPORTAUDIO_DIR}")
 
-	find_library(LIBPORTAUDIO_LIBRARY NAMES portaudio portaudio19 portaudio2 libportaudio libportaudio19 libportaudio2
-		HINTS ${LIBPORTAUDIO_DIR}/lib )
+	find_library(LIBPORTAUDIO_LIBRARY
+		NAMES ${LIBPORTAUDIO_SEARCH_LIBS}
+		PATHS ${_subdirs})
 
 #	message("LIBPORTAUDIO_LIBRARY: ${LIBPORTAUDIO_LIBRARY}")
 
-	find_path(LIBPORTAUDIO_INCLUDE_DIR portaudio.h
-		HINTS ${LIBPORTAUDIO_DIR}/include
-		PATH_SUFFIXES portaudio )
+	find_path(LIBPORTAUDIO_INCLUDE_DIR ${LIBPORTAUDIO_SEARCH_HEADERS}
+		PATHS ${_subdirs}
+		PATH_SUFFIXES include portaudio include/portaudio)
 
 #	message("LIBPORTAUDIO_INCLUDE_DIR: ${LIBPORTAUDIO_LIBRARY}")
 
@@ -32,21 +44,21 @@ else ()
 	set(LIBPORTAUDIO_DEFINITIONS ${PC_LIBPORTAUDIO_CFLAGS_OTHER})
 
 	find_path(LIBPORTAUDIO_INCLUDE_DIR portaudio.h
-          HINTS ${PC_LIBPORTAUDIO_INCLUDEDIR} ${PC_LIBPORTAUDIO_INCLUDE_DIRS}
-          PATH_SUFFIXES portaudio )
+        HINTS ${PC_LIBPORTAUDIO_INCLUDEDIR} ${PC_LIBPORTAUDIO_INCLUDE_DIRS}
+        PATH_SUFFIXES portaudio)
 
 	find_library(LIBPORTAUDIO_LIBRARY NAMES libportaudio portaudio
-             HINTS ${PC_LIBPORTAUDIO_LIBDIR} ${PC_LIBPORTAUDIO_LIBRARY_DIRS} )
+        HINTS ${PC_LIBPORTAUDIO_LIBDIR} ${PC_LIBPORTAUDIO_LIBRARY_DIRS})
 
 endif()
 
-set(LIBPORTAUDIO_LIBRARIES ${LIBPORTAUDIO_LIBRARY} )
-set(LIBPORTAUDIO_INCLUDE_DIRS ${LIBPORTAUDIO_INCLUDE_DIR} )
+set(LIBPORTAUDIO_LIBRARIES ${LIBPORTAUDIO_LIBRARY})
+set(LIBPORTAUDIO_INCLUDE_DIRS ${LIBPORTAUDIO_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set LIBPORTAUDIO_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(Portaudio  DEFAULT_MSG
-                                  LIBPORTAUDIO_LIBRARIES LIBPORTAUDIO_INCLUDE_DIRS)
+find_package_handle_standard_args(LIBPORTAUDIO  DEFAULT_MSG
+                        LIBPORTAUDIO_LIBRARIES LIBPORTAUDIO_INCLUDE_DIRS)
 
 mark_as_advanced(LIBPORTAUDIO_INCLUDE_DIRS LIBPORTAUDIO_LIBRARIES)
