13c46281c7ec02dcf3cf204b2fce7f8dcb762d8e
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2016-08-26 12:53:33 +0200

    CMakeCompatibility.cmake: switch CMP0028 to NEW SetupVersion.cmake: add setup_version() macro

--- cmake/CMakeCompatibility.cmake
+++ cmake/CMakeCompatibility.cmake
@@ -8,10 +8,15 @@ if (POLICY CMP0063)
 	# Honor visibility properties for all target types.
 	cmake_policy(SET CMP0063 NEW)
 endif()
+if (POLICY CMP0028)
+	# recognize target names which use double colon as IMPORTED or ALIAS
+	cmake_policy(SET CMP0028 NEW)
+endif()
 
 if (NOT DEFINED CMAKE_RUNTIME_OUTPUT_DIRECTORY)
 	set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin CACHE
 		PATH "Directory where .exe and .dll files are dumped.")
+	mark_as_advanced(CMAKE_RUNTIME_OUTPUT_DIRECTORY)
 endif()
 
 if(${CMAKE_VERSION} VERSION_LESS 2.8.11)
@@ -49,3 +54,4 @@ function(target_compile_definitions _TARGET)
 	_set_target_defs(${_TARGET} _defs_PRIVATE)
 endfunction()
 endif()
+
