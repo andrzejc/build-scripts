d5206445f3eac8c1e473073e7a8c56a211dd1b1c
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-02-24 23:32:51 +0100

    WIP

--- ci/install-test-deps.windows.sh
+++ ci/install-test-deps.windows.sh
@@ -20,29 +20,12 @@ choco install ninja
 # mv "/C/Program Files/Mega-Nerd/libsndfile/lib/libsndfile-1.dll" \
 #     "/C/Program Files/Mega-Nerd/libsndfile/bin"
 
-function install_libsndfile {
-    local url="$1"
-    local hash="$2"
-    local installer_file=
-    local temp_dir=
-    temp_dir=$( mktemp -d )
-    trap "rm -rf '${temp_dir}'" EXIT
-    installer_file=$( bin/safe-download "${url}" "${temp_dir}" "${hash}" )
-    # HACK: installer shows modal dialog box from subprocess sndfile-about.exe asking to donate
-    # (thank you, Erik de Castro Lopo, this is great for CI), but this is Windows
-    # native process and we can't get its pid (tasklist shows truncated commands so it doesn't help).
-    # Just wait a bit and kill the parent.
-    MSYS_NO_PATHCONV=1 "${installer_file}" /VERYSILENT &
-    local pid=$!
-    sleep 10
-    kill "${pid}"
-}
 
 case "${TARGET_PLATFORM:-x64}" in
 x64)
-    install_libsndfile http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w64-setup.exe 3783e513d735d1526f19a32a63991026 ;;
+    bin/winstall-libsndfile http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w64-setup.exe 3783e513d735d1526f19a32a63991026 ;;
 Win32)
-    install_libsndfile http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w32-setup.exe 443a2a2890969778e8f9fe6a146c0595 ;;
+    bin/winstall-libsndfile http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w32-setup.exe 443a2a2890969778e8f9fe6a146c0595 ;;
 *)
     >&2 echo "Installation of libsndfile for this target is not supported"
     return 1 ;;
