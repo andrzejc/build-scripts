ef1394924ae91aaac1672fd1e3c553798c9d7116
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-02-24 13:50:55 +0100

    WIP

--- ci/install-test-deps.windows.sh
+++ ci/install-test-deps.windows.sh
@@ -2,18 +2,23 @@
 # unusable for use on CI because it shows "donate or f**k off" dialog box
 
 # Emulate installation of libsndfile via binary installer
-bin/safe-download \
-    https://github.com/bastibe/libsndfile-binaries/archive/1.0.27.tar.gz \
+# bin/safe-download \
+#     https://github.com/bastibe/libsndfile-binaries/archive/1.0.27.tar.gz \
+#     install \
+#     7886e3d381020f2374e272652c3438c2
+# tar -xf install/1.0.27.tar.gz -C install
+# mkdir -p "/C/Program Files/Mega-Nerd/libsndfile/lib" \
+#     "/C/Program Files/Mega-Nerd/libsndfile/include" \
+#     "/C/Program Files/Mega-Nerd/libsndfile/bin"
+# cp install/libsndfile-binaries-1.0.27/libsndfile64bit.dll \
+#     "/C/Program Files/Mega-Nerd/libsndfile/lib/libsndfile-1.dll"
+# bin/dll2lib "/C/Program Files/Mega-Nerd/libsndfile/lib/libsndfile-1.dll" x64
+# tar -xf test/deps/libsndfile1-win64-include.tar.bz2 \
+#     -C "/C/Program Files/Mega-Nerd/libsndfile/include"
+# mv "/C/Program Files/Mega-Nerd/libsndfile/lib/libsndfile-1.dll" \
+#     "/C/Program Files/Mega-Nerd/libsndfile/bin"
+bin/safe-download http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w32-setup.exe \
     install \
-    7886e3d381020f2374e272652c3438c2
-tar -xf install/1.0.27.tar.gz -C install
-mkdir -p "/C/Program Files/Mega-Nerd/libsndfile/lib" \
-    "/C/Program Files/Mega-Nerd/libsndfile/include" \
-    "/C/Program Files/Mega-Nerd/libsndfile/bin"
-cp install/libsndfile-binaries-1.0.27/libsndfile64bit.dll \
-    "/C/Program Files/Mega-Nerd/libsndfile/lib/libsndfile-1.dll"
-bin/dll2lib "/C/Program Files/Mega-Nerd/libsndfile/lib/libsndfile-1.dll" x64
-tar -xf test/deps/libsndfile1-win64-include.tar.bz2 \
-    -C "/C/Program Files/Mega-Nerd/libsndfile/include"
-mv "/C/Program Files/Mega-Nerd/libsndfile/lib/libsndfile-1.dll" \
-    "/C/Program Files/Mega-Nerd/libsndfile/bin"
+    f17f4d2e14d332b88b4ab9847d489f44e9e3406b
+# HACK: installer shows modal dialog box from subprocess sndfile-about.exe
+install/libsndfile-1.0.28-w32-setup.exe /VERYSILENT
--- cmake/FindSndfile.cmake
+++ cmake/FindSndfile.cmake
@@ -5,6 +5,25 @@
 #  SndFile_LIBRARIES - The libraries needed to use libsndfile
 #  SndFile_DEFINITIONS - Compiler switches required for using libsndfile
 
+if(MSVC)
+    find_library(SNDFILE_LIBS libsndfile-1 HINTS "${CMAKE_SOURCE_DIR}/deps")
+    find_path(SNDFILE_INCLUDE_DIR sndfile.h HINTS "${CMAKE_SOURCE_DIR}/deps")
+else()
+    find_library(SNDFILE_LIBS sndfile)
+    find_path(SNDFILE_INCLUDE_DIR sndfile.h)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Sndfile REQUIRED_VARS SNDFILE_LIBS SNDFILE_INCLUDE_DIR)
+
+if(Sndfile_FOUND AND NOT TARGET Sndfile::libsndfile)
+    add_library(Sndfile::libsndfile INTERFACE IMPORTED)
+    set_target_properties(Sndfile::libsndfile PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${SNDFILE_INCLUDE_DIR}"
+        INTERFACE_LINK_LIBRARIES "${SNDFILE_LIBS}"
+    )
+endif()
+
 set(SndFile_SEARCH_LIBS libsndfile sndfile libsndfile-1 sndfile-1
 	libsndfile1 sndfile1)
 set(SndFile_SEARCH_HEADERS sndfile.h)
