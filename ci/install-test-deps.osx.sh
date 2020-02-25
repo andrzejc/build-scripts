e14a0653a4812a535bdbf54717d30aea5e9a36db
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-02-25 21:45:17 +0100

    WIP

--- ci/install-test-deps.osx.sh
+++ ci/install-test-deps.osx.sh
@@ -1,3 +1,3 @@
 HOMEBREW_NO_AUTO_UPDATE=1 \
 HOMEBREW_NO_INSTALL_CLEANUP=1 \
-    brew install ninja libsndfile portaudio
+    brew install libsndfile portaudio
--- ci/install-test-deps.windows.sh
+++ ci/install-test-deps.windows.sh
@@ -4,7 +4,6 @@ set -o pipefail
 
 # Required for safe-download script
 choco install openssl.light
-choco install ninja
 
 bin/winstall-portaudio "${TARGET_PLATFORM}"
 
@@ -12,11 +11,9 @@ case "${TARGET_PLATFORM}" in
 *64)
     bin/winstall-libsndfile http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w64-setup.exe 3783e513d735d1526f19a32a63991026 ;;
 
-*32|*86)
+*in32|*86)
     bin/winstall-libsndfile http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w32-setup.exe 443a2a2890969778e8f9fe6a146c0595 ;;
 *)
     >&2 echo "Installation of libsndfile for target '${TARGET_PLATFORM}' is not supported"
     return 1 ;;
 esac
-
-source bin/vcvarsall "${TARGET_PLATFORM}"
