bbbe147d52ccc7ed6ccc2687a4b11a5cc88df20f
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-02-25 18:23:10 +0100

    WIP

--- ci/install-test-deps.windows.sh
+++ ci/install-test-deps.windows.sh
@@ -1,21 +1,22 @@
 
+set -e
+set -o pipefail
+
 # Required for safe-download script
 choco install openssl.light
 choco install ninja
 
-bin/winstall-portaudio "${TARGET_PLATFORM:-x64}"
+bin/winstall-portaudio "${TARGET_PLATFORM}"
 
-case "${TARGET_PLATFORM:-x64}" in
-x64)
-    VARS=amd64
+case "${TARGET_PLATFORM}" in
+*64)
     bin/winstall-libsndfile http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w64-setup.exe 3783e513d735d1526f19a32a63991026 ;;
 
-Win32)
-    VARS=x86
+*32|*86)
     bin/winstall-libsndfile http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w32-setup.exe 443a2a2890969778e8f9fe6a146c0595 ;;
 *)
-    >&2 echo "Installation of libsndfile for this target is not supported"
+    >&2 echo "Installation of libsndfile for target '${TARGET_PLATFORM}' is not supported"
     return 1 ;;
 esac
 
-source bin/vcvarsall ${VARS}
+source bin/vcvarsall "${TARGET_PLATFORM}"
