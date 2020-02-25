60eca9790c5b330a848b2390872e0489dd869773
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-02-25 01:23:43 +0100

    WIP portaudio

---
+++ bin/winstall-portaudio.cmd
@@ -0,0 +1,2 @@
+@echo off
+bash %~dp0winstall-portaudio %*
--- ci/install-test-deps.windows.sh
+++ ci/install-test-deps.windows.sh
@@ -3,23 +3,7 @@
 choco install openssl.light
 choco install ninja
 
-# Emulate installation of libsndfile via binary installer
-# bin/safe-download \
-#     https://github.com/bastibe/libsndfile-binaries/archive/1.0.27.tar.gz \
-#     install \
-#     7886e3d381020f2374e272652c3438c2
-# tar -xf install/1.0.27.tar.gz -C install
-# mkdir -p "/C/Program Files/Mega-Nerd/libsndfile/lib" \
-#     "/C/Program Files/Mega-Nerd/libsndfile/include" \
-#     "/C/Program Files/Mega-Nerd/libsndfile/bin"
-# cp install/libsndfile-binaries-1.0.27/libsndfile64bit.dll \
-#     "/C/Program Files/Mega-Nerd/libsndfile/lib/libsndfile-1.dll"
-# bin/dll2lib "/C/Program Files/Mega-Nerd/libsndfile/lib/libsndfile-1.dll" x64
-# tar -xf test/deps/libsndfile1-win64-include.tar.bz2 \
-#     -C "/C/Program Files/Mega-Nerd/libsndfile/include"
-# mv "/C/Program Files/Mega-Nerd/libsndfile/lib/libsndfile-1.dll" \
-#     "/C/Program Files/Mega-Nerd/libsndfile/bin"
-
+bin/winstall-portaudio "${TARGET_PLATFORM:-x64}"
 
 case "${TARGET_PLATFORM:-x64}" in
 x64)
