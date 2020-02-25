5298132a0a28fd1b4daec38a45f2acec204afece
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-02-25 17:05:43 +0100

    WIP

--- bin/_vcvarsall-internal.cmd
+++ bin/_vcvarsall-internal.cmd
@@ -1,5 +1,4 @@
 @echo off
 call "%VS140COMNTOOLS%..\..\VC\vcvarsall.bat" %* >&2
 
-REM bash -c "declare -p ........"
-env
+bash -c "declare -p ExtensionSdkDir Framework40Version FrameworkDir FrameworkDIR64 FrameworkVersion FrameworkVersion64 INCLUDE LIB LIBPATH NETFXSDKDir PATH Platform UCRTVersion UniversalCRTSdkDir VCINSTALLDIR VisualStudioVersion VSINSTALLDIR WindowsLibPath WindowsSdkDir WindowsSDKLibVersion WindowsSDKVersion WindowsSDK_ExecutablePath_x64 WindowsSDK_ExecutablePath_x86"
--- ci/install-test-deps.windows.sh
+++ ci/install-test-deps.windows.sh
@@ -7,13 +7,16 @@ bin/winstall-portaudio "${TARGET_PLATFORM:-x64}"
 
 case "${TARGET_PLATFORM:-x64}" in
 x64)
+    VARS=amd64
     bin/winstall-libsndfile http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w64-setup.exe 3783e513d735d1526f19a32a63991026 ;;
+
 Win32)
+    VARS=x86
     bin/winstall-libsndfile http://www.mega-nerd.com/libsndfile/files/libsndfile-1.0.28-w32-setup.exe 443a2a2890969778e8f9fe6a146c0595 ;;
 *)
     >&2 echo "Installation of libsndfile for this target is not supported"
     return 1 ;;
 esac
 
-
-MSYS_NO_PATHCONV=1 source bin/vcvarsall amd64
+source bin/vcvarsall ${VARS}
+env
